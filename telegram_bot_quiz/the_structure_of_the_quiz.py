c7707091e1b3a40161da1836a69db75af46ff656
Maksim <112460508+mitsapov@users.noreply.github.com>
2024-12-07 11:01:49 +0700

    the_structure_of_the_quiz.py

--- telegram_bot_quiz/the_structure_of_the_quiz.py
+++ telegram_bot_quiz/the_structure_of_the_quiz.py
@@ -1,52 +1,52 @@
 quiz_data = [
     {
-        'question': 'Что такое Python?',
-        'options': ['Язык программирования', 'Тип данных', 'Музыкальный инструмент', 'Змея на английском'],
+        'question': 'What is Python?',
+        'options': ['Programming language', 'Data type', 'Musical instrument', 'Snake in English'],
         'correct_option': 0
     },
     {
-        'question': 'Какой тип данных используется для хранения целых чисел?',
+        'question': 'What data type is used to store integers?',
         'options': ['int', 'float', 'str', 'natural'],
         'correct_option': 0
     },
     {
-        'question': 'Как называется основной интерпретатор Python?',
+        'question': 'What is the name of the main Python interpreter?',
         'options': ['Python', 'Cython', 'PyPy', 'CPython'],
         'correct_option': 0
     },
     {
-        'question': 'Что такое PEP в контексте Python?',
-        'options': ['Пакет расширений Python', 'Предложение по улучшению Python', 'Программа для редактирования Python-кода', 'Плагин для Python'],
+        'question': 'What is a PEP in the context of Python?',
+        'options': ['Python Extension Package', 'Python Enhancement Proposal', 'Python code editor program', 'Plugin for Python'],
         'correct_option': 1
     },
     {
-        'question': 'Какой оператор используется для сравнения двух значений в Python?',
+        'question': 'What operator is used to compare two values in Python?',
         'options': ['==', '<>', '<=', '>='],
         'correct_option': 0
     },
     {
-        'question': 'Какая функция используется для вывода текста в консоль в Python?',
+        'question': 'What function is used to print text to the console in Python?',
         'options': ['print()', 'show()', 'output()', 'console()'],
         'correct_option': 0
     },
     {
-        'question': 'Какой тип данных используется для хранения дробных чисел?',
+        'question': 'What data type is used to store floating-point numbers?',
         'options': ['int', 'float', 'str', 'natural'],
         'correct_option': 2
     },
     {
-        'question': 'Что такое цикл for в Python?',
-        'options': ['Оператор, который позволяет выполнять код многократно', 'Функция, которая возвращает значение', 'Условный оператор, который проверяет условие', 'Метод, который изменяет значение переменной'],
+        'question': 'What is a for loop in Python?',
+        'options': ['An operator that allows executing code многократно', 'A function that returns a value', 'A conditional statement that checks a condition', 'A method that changes a variable\'s value'],
         'correct_option': 0
     },
     {
-        'question': 'Что такое список в Python?',
-        'options': ['Тип данных, который хранит упорядоченный набор элементов', 'Оператор, который выполняет код многократно', 'Функция, которая выводит текст в консоль', 'Объект, который представляет собой строку'],
+        'question': 'What is a list in Python?',
+        'options': ['A data type that stores an ordered collection of elements', 'An operator that executes code многократно', 'A function that prints text to the console', 'An object that represents a string'],
         'correct_option': 0
     },
     {
-        'question': 'Что такое словарь в Python?',
-        'options': ['Тип данных, который хранит неупорядоченный набор пар ключ-значение', 'Оператор, который сравнивает два значения', 'Функция, которая принимает аргументы', 'Объект, который является экземпляром класса'],
+        'question': 'What is a dictionary in Python?',
+        'options': ['A data type that stores an unordered collection of key-value pairs', 'An operator that compares two values', 'A function that takes arguments', 'An object that is an instance of a class'],
         'correct_option': 0
     }
 ]
