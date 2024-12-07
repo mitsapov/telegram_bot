5deb2669e3d394452127a5fc8f61ba1000386c78
Maksim <112460508+mitsapov@users.noreply.github.com>
2024-12-07 10:58:51 +0700

    creating_database.py

--- telegram_bot_quiz/creating_database.py
+++ telegram_bot_quiz/creating_database.py
@@ -1,34 +1,34 @@
 import aiosqlite
 
-# Зададим имя базы данных
+# Let's set the name of the database
 DB_NAME = 'quiz_bot.db'
 
 async def create_table():
-    # Создаем соединение с базой данных (если она не существует, то она будет создана)
+    # Creating a connection to the database (if it does not exist, it will be created)
     async with aiosqlite.connect('quiz_bot.db') as db:
-        # Выполняем SQL-запрос к базе данных
+        # Executing an SQL query to the database
         await db.execute('''CREATE TABLE IF NOT EXISTS quiz_state (
             user_id INTEGER PRIMARY KEY,
             question_index INTEGER,
             right_answers INTEGER
         )''')
-        # Сохраняем изменения
+        # Saving the changes
         await db.commit()
 
 async def update_quiz_index(user_id, index):
-    # Создаем соединение с базой данных (если она не существует, она будет создана)
+    # Creating a connection to the database (if it does not exist, it will be created)
     async with aiosqlite.connect(DB_NAME) as db:
-        # Вставляем новую запись или заменяем ее, если с данным user_id уже существует
+        # Insert a new record or replace it if the given user_id already exists
         await db.execute('INSERT OR REPLACE INTO quiz_state (user_id, question_index, right_answers) VALUES (?, ?, ?)', (user_id, index, 0))
-        # Сохраняем изменения
+        # Saving the changes
         await db.commit()
 
 async def get_quiz_index(user_id):
-    # Подключаемся к базе данных
+    # Connecting to the database
     async with aiosqlite.connect(DB_NAME) as db:
-        # Получаем запись для заданного пользователя
+        # Getting an entry for a given user
         async with db.execute('SELECT question_index FROM quiz_state WHERE user_id = (?)', (user_id, )) as cursor:
-            # Возвращаем результат
+            # Returning the result
             results = await cursor.fetchone()
             if results is not None:
                 return results[0]
@@ -36,22 +36,22 @@ async def get_quiz_index(user_id):
                 return 0
 
 async def delete_previous_result(user_id):
-    # Подключаемся к базе данных
+    # Connecting to the database
     async with aiosqlite.connect(DB_NAME) as db:
-        # Удаляем предыдущую запись для данного пользователя
+        # Deleting the previous record for this user
         await db.execute('DELETE FROM quiz_state WHERE user_id = ?', (user_id, ))
-        # Сохраняем изменения
+        # Saving the changes
         await db.commit()
 
 async def update_right_answers(user_id, right_answers):
-    # Подключаемся к базе данных
+    # Connecting to the database
     async with aiosqlite.connect(DB_NAME) as db:
-        # Обновляем количество правильных ответов
+        # Updating the number of correct answers
         await db.execute('UPDATE quiz_state SET right_answers = ? WHERE user_id = ?', (right_answers, user_id))
-        # Сохраняем изменения
+        # Saving the changes
         await db.commit()
 
-# Получение количества правильных ответов для данного пользователя
+# Getting the number of correct answers for a given user
 async def get_result(user_id):
     async with aiosqlite.connect(DB_NAME) as db:
         async with db.execute('SELECT right_answers FROM quiz_state WHERE user_id = (?)', (user_id, )) as cursor:
