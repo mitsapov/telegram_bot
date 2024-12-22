28e4c33702b28a42c78ff2cb9135e68250d47a13
mitsapov <maksimcapov@gmail.com>
2024-12-22 19:22:53 +0700

    new file

---
+++ quiz/creating_database.py
@@ -0,0 +1,62 @@
+import aiosqlite
+
+# Let's set the name of the database
+DB_NAME = ' '
+
+async def create_table():
+    # Creating a connection to the database (if it does not exist, it will be created)
+    async with aiosqlite.connect('quiz_bot.db') as db:
+        # Executing an SQL query to the database
+        await db.execute('''CREATE TABLE IF NOT EXISTS quiz_state (
+            user_id INTEGER PRIMARY KEY,
+            question_index INTEGER,
+            right_answers INTEGER
+        )''')
+        # Saving the changes
+        await db.commit()
+
+async def update_quiz_index(user_id, index):
+    # Creating a connection to the database (if it does not exist, it will be created)
+    async with aiosqlite.connect(DB_NAME) as db:
+        # Insert a new record or replace it if the given user_id already exists
+        await db.execute('INSERT OR REPLACE INTO quiz_state (user_id, question_index, right_answers) VALUES (?, ?, ?)', (user_id, index, 0))
+        # Saving the changes
+        await db.commit()
+
+async def get_quiz_index(user_id):
+    # Connecting to the database
+    async with aiosqlite.connect(DB_NAME) as db:
+        # Getting an entry for a given user
+        async with db.execute('SELECT question_index FROM quiz_state WHERE user_id = (?)', (user_id, )) as cursor:
+            # Returning the result
+            results = await cursor.fetchone()
+            if results is not None:
+                return results[0]
+            else:
+                return 0
+
+async def delete_previous_result(user_id):
+    # Connecting to the database
+    async with aiosqlite.connect(DB_NAME) as db:
+        # Deleting the previous record for this user
+        await db.execute('DELETE FROM quiz_state WHERE user_id = ?', (user_id, ))
+        # Saving the changes
+        await db.commit()
+
+async def update_right_answers(user_id, right_answers):
+    # Connecting to the database
+    async with aiosqlite.connect(DB_NAME) as db:
+        # Updating the number of correct answers
+        await db.execute('UPDATE quiz_state SET right_answers = ? WHERE user_id = ?', (right_answers, user_id))
+        # Saving the changes
+        await db.commit()
+
+# Getting the number of correct answers for a given user
+async def get_result(user_id):
+    async with aiosqlite.connect(DB_NAME) as db:
+        async with db.execute('SELECT right_answers FROM quiz_state WHERE user_id = (?)', (user_id, )) as cursor:
+            results = await cursor.fetchone()
+            if results is not None:
+                return results[0]
+            else:
+                return 0
---
+++ quiz/the_structure_of_the_quiz.py
@@ -0,0 +1,52 @@
+quiz_data = [
+    {
+        'question': 'What is Python?',
+        'options': ['Programming language', 'Data type', 'Musical instrument', 'Snake in English'],
+        'correct_option': 0
+    },
+    {
+        'question': 'What data type is used to store integers?',
+        'options': ['int', 'float', 'str', 'natural'],
+        'correct_option': 0
+    },
+    {
+        'question': 'What is the name of the main Python interpreter?',
+        'options': ['Python', 'Cython', 'PyPy', 'CPython'],
+        'correct_option': 0
+    },
+    {
+        'question': 'What is a PEP in the context of Python?',
+        'options': ['Python Extension Package', 'Python Enhancement Proposal', 'Python code editor program', 'Plugin for Python'],
+        'correct_option': 1
+    },
+    {
+        'question': 'What operator is used to compare two values in Python?',
+        'options': ['==', '<>', '<=', '>='],
+        'correct_option': 0
+    },
+    {
+        'question': 'What function is used to print text to the console in Python?',
+        'options': ['print()', 'show()', 'output()', 'console()'],
+        'correct_option': 0
+    },
+    {
+        'question': 'What data type is used to store floating-point numbers?',
+        'options': ['int', 'float', 'str', 'natural'],
+        'correct_option': 2
+    },
+    {
+        'question': 'What is a for loop in Python?',
+        'options': ['An operator that allows executing code многократно', 'A function that returns a value', 'A conditional statement that checks a condition', 'A method that changes a variable\'s value'],
+        'correct_option': 0
+    },
+    {
+        'question': 'What is a list in Python?',
+        'options': ['A data type that stores an ordered collection of elements', 'An operator that executes code многократно', 'A function that prints text to the console', 'An object that represents a string'],
+        'correct_option': 0
+    },
+    {
+        'question': 'What is a dictionary in Python?',
+        'options': ['A data type that stores an unordered collection of key-value pairs', 'An operator that compares two values', 'A function that takes arguments', 'An object that is an instance of a class'],
+        'correct_option': 0
+    }
+]
