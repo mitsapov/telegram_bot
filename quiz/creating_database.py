b57647d397798d1ccde8bf2c057784ab590d02aa
mitsapov <maksimcapov@gmail.com>
2025-05-10 16:48:17 +0700

    Bot Updates:     Added Rating System     Implemented collection and storage of participants’ results     When re-participating, the previous result of the participant is annulled     User Identification System     Added mandatory name input before starting the quiz     Implemented name uniqueness check     The system automatically recognizes existing users by ID and does not re-request the name     Results Visualization     Added functionality to display the top 5 best participants

--- quiz/creating_database.py
+++ quiz/creating_database.py
@@ -1,32 +1,38 @@
 import aiosqlite
 
-# Let's set the name of the database
-DB_NAME = ' '
+# Setting a name for the database
+DB_NAME = 'NAME_BD.db'
 
+# Creating a table
 async def create_table():
-    # Creating a connection to the database (if it does not exist, it will be created)
-    async with aiosqlite.connect('quiz_bot.db') as db:
-        # Executing an SQL query to the database
+    async with aiosqlite.connect('NAME_BD.db') as db:
         await db.execute('''CREATE TABLE IF NOT EXISTS quiz_state (
             user_id INTEGER PRIMARY KEY,
             question_index INTEGER,
-            right_answers INTEGER
+            right_answers INTEGER,
+            username TEXT UNIQUE
         )''')
-        # Saving the changes
         await db.commit()
 
+# Managing the user's status
 async def update_quiz_index(user_id, index):
-    # Creating a connection to the database (if it does not exist, it will be created)
     async with aiosqlite.connect(DB_NAME) as db:
-        # Insert a new record or replace it if the given user_id already exists
-        await db.execute('INSERT OR REPLACE INTO quiz_state (user_id, question_index, right_answers) VALUES (?, ?, ?)', (user_id, index, 0))
-        # Saving the changes
+        # Checking if the record exists
+        cursor = await db.execute('SELECT right_answers FROM quiz_state WHERE user_id = ?', (user_id,))
+        result = await cursor.fetchone()
+        if result is None:
+            # If there is no record, create a new one with right_answers = 0
+            await db.execute('INSERT INTO quiz_state (user_id, question_index, right_answers) VALUES (?, ?, ?)', (user_id, index, 0))
+        else:
+            # If there is an entry, we update only question_index.
+            await db.execute('UPDATE quiz_state SET question_index = ? WHERE user_id = ?', (index, user_id))
         await db.commit()
 
+# Defining the current stage of the quiz by the user
 async def get_quiz_index(user_id):
     # Connecting to the database
     async with aiosqlite.connect(DB_NAME) as db:
-        # Getting an entry for a given user
+        # Getting a record for this user
         async with db.execute('SELECT question_index FROM quiz_state WHERE user_id = (?)', (user_id, )) as cursor:
             # Returning the result
             results = await cursor.fetchone()
@@ -35,23 +41,20 @@ async def get_quiz_index(user_id):
             else:
                 return 0
 
+# We reset the user's previous result
 async def delete_previous_result(user_id):
-    # Connecting to the database
     async with aiosqlite.connect(DB_NAME) as db:
-        # Deleting the previous record for this user
-        await db.execute('DELETE FROM quiz_state WHERE user_id = ?', (user_id, ))
-        # Saving the changes
+        # Updating question_index and right_answers, but keeping username
+        await db.execute('UPDATE quiz_state SET question_index = 0, right_answers = 0 WHERE user_id = ?', (user_id,))
         await db.commit()
 
-async def update_right_answers(user_id, right_answers):
-    # Connecting to the database
+# Increasing the number of correct user responses
+async def update_right_answers(user_id):
     async with aiosqlite.connect(DB_NAME) as db:
-        # Updating the number of correct answers
-        await db.execute('UPDATE quiz_state SET right_answers = ? WHERE user_id = ?', (right_answers, user_id))
-        # Saving the changes
+        await db.execute('UPDATE quiz_state SET right_answers = right_answers + 1 WHERE user_id = ?', (user_id,))
         await db.commit()
 
-# Getting the number of correct answers for a given user
+# We get the number of correct user responses
 async def get_result(user_id):
     async with aiosqlite.connect(DB_NAME) as db:
         async with db.execute('SELECT right_answers FROM quiz_state WHERE user_id = (?)', (user_id, )) as cursor:
@@ -60,3 +63,20 @@ async def get_result(user_id):
                 return results[0]
             else:
                 return 0
+
+# We will get the top 5 participants in terms of the number of correct answers
+async def get_top_participants():
+    async with aiosqlite.connect(DB_NAME) as db:
+        # We get the top 5 participants with names
+        cursor = await db.execute('SELECT username, right_answers FROM quiz_state ORDER BY right_answers DESC LIMIT 5')
+        participant_list = []
+        for row in await cursor.fetchall():
+            username, right_answers = row
+            participant_list.append(f"{username}: {right_answers} correct answers")
+        
+        # If the list is empty, we return a message about missing data.
+        if not participant_list:
+            return "The participant rating is empty. No one has passed the quiz yet."
+        
+        # Возвращаем отформатированный список
+        return "Top 5 participants:\n" + "\n".join(participant_list)
